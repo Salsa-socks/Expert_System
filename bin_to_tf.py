5d72c1371157b68f4348f3dda8c80cc884c54252
bnkosi2019 <47521522+bnkosi2019@users.noreply.github.com>
2020-01-13 14:08:08 +0200

    need input via command line or file

---
+++ bin_to_tf.py
@@ -0,0 +1,17 @@
+# bnkosi
+# Jan 2020
+
+var_list = ['a','b','c','d']
+line = []
+num_of_rows = len(var_list) ** 2
+widest_num = len(str(bin(num_of_rows - 1)[2:]))
+
+for i in reversed(range(num_of_rows)):
+    current_bin = bin(i)[2:].zfill(widest_num)
+    for letter in str(current_bin):
+        if letter == '0':
+            line.append('False')
+        elif letter == '1':
+            line.append('True')
+
+print(line)         
